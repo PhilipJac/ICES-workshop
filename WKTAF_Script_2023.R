3456d9afd1900afd01af88c369733d3f53d05a65
Philip Jacobson <philip.jacobson@slu.se>
2023-11-28 08:52:51 +0100

    Testa att det funkar

---
+++ WKTAF_Script_2023.R
@@ -0,0 +1,22 @@
+#### WKTAF 2023-11-28 - 2023-11-29 ####
+
+#### install packages accroding to e-mail
+
+#install.packages("icesTAF") 
+#install.packages("icesConnect") 
+#install.packages("stockassessment", repos = "https://fishfollower.r-universe.dev") 
+#install.packages("TMB", type = "source")
+#install.packages("ellipse")
+#install.packages("cat3advice", repos = "https://ices-tools-prod.r-universe.dev") 
+#install.packages("usethis")
+
+# Load packages
+
+library(icesTAF)
+library(icesConnect)
+library(stockassessment)
+library(cat3advice)
+library(usethis)
+
+
+1+1
